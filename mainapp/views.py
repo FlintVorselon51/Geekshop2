ddf16a39ccf862de47f433930670618fea2deab3
hitasher <hitasher@gmail.com>
2021-07-19 16:21:32 +0400

    Add cache_page for product view

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,6 +4,7 @@ import random
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.cache import cache_page
 
 from mainapp.models import ProductCategory, Product
 from django.conf import settings
@@ -107,7 +108,8 @@ def products(request, pk=None, page=1):
     
     return render(request, 'mainapp/products.html', content)
     
-    
+
+@cache_page(3600)
 def product(request, pk):
     title = 'продукты'
     links_menu = get_links_menu()
